4ed68cc99feea276bb000e1a310c95392bffe3f4
Salomão Cruz <salomao.dacostacruz@gmail.com>
2022-05-03 09:02:58 -0400

    Página de cadastro - frontend

--- cadastro.php
+++ cadastro.php
@@ -1,10 +1,11 @@
 <?php
-	if (isset($_POST['register'])) {
-		$pdo = new PDO('mysql:host=localhost;dbname=dados_clientes', 'root', '');
-		$query = $pdo->prepare('SELECT * FROM tipos');
-		$query->execute();
-		$types = $query->fetchAll();
-	}	
+	$pdo = new PDO('mysql:host=localhost;dbname=dados_clientes', 'root', '');
+	$query = $pdo->prepare('SELECT * FROM tipos');
+	$query->execute();
+	$types = $query->fetchAll();
+	$query = $pdo->prepare('SELECT * FROM motivos');
+	$query->execute();
+	$justifications = $query->fetchAll();
 ?>
 <html>
 <head>
@@ -14,30 +15,43 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<script src="https://unpkg.com/jspdf@latest/dist/jspdf.umd.min.js"></script>
 	<link rel="stylesheet" href="./css/style.css"/>
+	<script>
+		const verifyCheckbox = (event) => {
+			const check_boxes = document.querySelectorAll('[type="checkbox"]')
+			let one_checked = false
+			for (const checkbox of check_boxes) {
+				if (checkbox.checked === true) one_checked = true
+			}
+			if (!one_checked) {
+				event.preventDefault()
+				alert('Escolha as justificativas')
+			}
+		}
+	</script>
 </head>
 <body>
 	<div class="h-100 d-flex justify-content-center aling-items-center">
-		<div class="position-absolute align-self-center container w-25 p-5 border rounded">
+		<div class="position-absolute align-self-center container p-5 border" style="top: 0px; width: 700px;">
 			<div class="row w-100">
 				<h4 class="h4-register d-flex align-self-center">Cadastro de Usuário</h4>
 			</div>
 			<div class="row">
-				<form class="form-register form-container" method="post" class="w-100 align-items-center d-flex">
+				<form class="form-register form-container ml-5 mr-5" method="post">
 					<div class="form-box row align-items-center w-100">
 							<label for="input-name" class="label-register">Nome completo</label>
-								<input name="name" id="input-name" class="w-75" type="text" placeholder="Ex: João Silva" required/>
+								<input name="name" id="input-name" class="w-100" type="text" placeholder="Ex: João Silva" required/>
 					</div>
 					<div class="form-box row align-items-center w-100">
-							<label for="input-username"  class="label-register">Nome de usuário</label>
-								<input name="username" id="input-username" class="w-75" type="text" placeholder="Ex: joaosilva" required/>
+							<label for="input-username"  class="label-register">Username</label>
+								<input name="username" id="input-username" class="w-100" type="text" placeholder="Ex: joaosilva" required/>
 					</div>
 					<div class="form-box row align-items-center w-100">
 						<label for="input-password" class="label-register">Senha</label>
-							<input name="password" id="input-password" class="w-75" name="password" type="text" placeholder="**********" required/>
+							<input name="password" id="input-password" class="w-100" name="password" type="text" placeholder="**********" required/>
 					</div>
 					<div class="form-box row align-items-center w-100">
 						<label for="select-usertype">Tipo de usuário</label>
-							<select name="usertype" id="select-usertype" class="w-75" required>
+							<select name="usertype" id="select-usertype" class="w-100" required>
 								<option value="">-- Selecione --</option>
 								<?php
 									foreach ($types as $type) {
@@ -46,17 +60,30 @@
 								?>
 							</select>
 					</div>
-					<div class="form-box row align-items-center w-100">
+					<div class="form-box row align-items-left">
 						<label>Justificativas para o usuário</label>
+						<div class="align-items-left">
+						<?php
+							$index = 1;
+							foreach ($justifications as $justification) {
+								echo '	
+									<div class="d-flex justify-content-left flex-direction-row align-items-center m-1">
+										<input type="checkbox" name="motivo-'.$index.'" id="motivo-'.$index.'" class="d-inline-flex" value="'.$justification['motivo'].'"/><label for="motivo-'.$index.'" class="m-1">'.$justification['motivo'].'</label>
+									</div>
+								';
+								$index++;
+							}
+						?>
+						</div>
 					</div>
 					<div class="form-box row align-items-center mt-4 w-100">
 						<div class="w-25 flex align-self-center">
-							<button name="register" type="submit" class="btn btn-success w-100 p-2">Cadastrar</button>
+							<button name="register" type="submit" class="btn btn-success w-100 p-2" onclick="verifyCheckbox(event)">Cadastrar</button>
 						</div>
 					</div>
 				</form>
 			</div>
 		</div>
 	</div>
-</body>
+</body>i
 </html>
