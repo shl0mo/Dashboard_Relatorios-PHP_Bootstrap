fff10332063bdf00fe78ed0aa065830fdd4d2cf6
Salomão Cruz <salomao.dacostacruz@gmail.com>
2022-05-03 02:24:00 -0400

    Página cadastro - front

--- cadastro.php
+++ cadastro.php
@@ -17,22 +17,20 @@
 			<h4 class="h4-register d-flex align-self-center">Cadastro de Usuário</h4>
 		</div>
 		<div class="row">
-			<form class="form-register" method="post" class="w-100 align-items-center d-flex">
-				<div>
-					<div class="d-flex flex-column align-items-center">
+			<form class="form-register form-container" method="post" class="w-100 align-items-center d-flex">
+				<div class="form-box row align-items-center">
 						<label for="input-name" class="label-register">Nome completo</label>
 							<input id="input-name" class="w-75" name="name" type="text"  placeholder="Ex: João Silva"/>
-					</div>
 				</div>
-				<div class="d-flex flex-column align-items-center">
-						<label for="input-username"  class="label-register mt-4">Nome de usuário</label>
+				<div class="form-box row align-items-center">
+						<label for="input-username"  class="label-register">Nome de usuário</label>
 							<input id="input-username" class="w-75" name="username" type="text" placeholder="Ex: joaosilva"/>
 				</div>
-				<div class="d-flex flex-column align-items-center">
+				<div class="form-box row align-items-center">
 					<label for="input-password"  class="label-register">Senha</label>
 						<input id="input-password" class="w-75" name="password" type="text" placeholder="**********"/>
 				</div>
-				<div class="d-flex flex-column align-items-center mt-4">
+				<div class="form-box row align-items-center mt-4">
 					<div class="w-25 flex align-self-center">
 						<button type="submit" class="btn btn-success w-100 p-2">Cadastrar</button>
 					</div>
