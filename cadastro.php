5651cfeb8ee24f5d1b539b6a8fe6f82ef77dd7a3
Salomão Cruz <salomao.dacostacruz@gmail.com>
2022-05-03 07:28:25 -0400

    Fetch dos tipos do banco de dados para a página de cadastro

--- cadastro.php
+++ cadastro.php
@@ -1,5 +1,10 @@
 <?php
-	
+	if (isset($_POST['register'])) {
+		$pdo = new PDO('mysql:host=localhost;dbname=dados_clientes', 'root', '');
+		$query = $pdo->prepare('SELECT * FROM tipos');
+		$query->execute();
+		$types = $query->fetchAll();
+	}	
 ?>
 <html>
 <head>
@@ -20,28 +25,33 @@
 				<form class="form-register form-container" method="post" class="w-100 align-items-center d-flex">
 					<div class="form-box row align-items-center w-100">
 							<label for="input-name" class="label-register">Nome completo</label>
-								<input id="input-name" class="w-75" name="name" type="text"  placeholder="Ex: João Silva"/>
+								<input name="name" id="input-name" class="w-75" type="text" placeholder="Ex: João Silva" required/>
 					</div>
 					<div class="form-box row align-items-center w-100">
 							<label for="input-username"  class="label-register">Nome de usuário</label>
-								<input id="input-username" class="w-75" name="username" type="text" placeholder="Ex: joaosilva"/>
+								<input name="username" id="input-username" class="w-75" type="text" placeholder="Ex: joaosilva" required/>
 					</div>
 					<div class="form-box row align-items-center w-100">
-						<label for="input-password"  class="label-register">Senha</label>
-							<input id="input-password" class="w-75" name="password" type="text" placeholder="**********"/>
+						<label for="input-password" class="label-register">Senha</label>
+							<input name="password" id="input-password" class="w-75" name="password" type="text" placeholder="**********" required/>
 					</div>
 					<div class="form-box row align-items-center w-100">
 						<label for="select-usertype">Tipo de usuário</label>
-							<select id="select-usertype" class="w-75">
+							<select name="usertype" id="select-usertype" class="w-75" required>
 								<option value="">-- Selecione --</option>
+								<?php
+									foreach ($types as $type) {
+										echo '<option>'.$type['tipo'].'</option>';
+									}
+								?>
 							</select>
 					</div>
 					<div class="form-box row align-items-center w-100">
-						<label>Justificativas para o usuário</label>	
+						<label>Justificativas para o usuário</label>
 					</div>
 					<div class="form-box row align-items-center mt-4 w-100">
 						<div class="w-25 flex align-self-center">
-							<button type="submit" class="btn btn-success w-100 p-2">Cadastrar</button>
+							<button name="register" type="submit" class="btn btn-success w-100 p-2">Cadastrar</button>
 						</div>
 					</div>
 				</form>
