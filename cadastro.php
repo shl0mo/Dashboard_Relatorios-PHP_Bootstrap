48be87932131def9c0802636a8a9b6baca82f136
Salomão Cruz <salomao.dacostacruz@gmail.com>
2022-05-03 00:52:49 -0400

    Página cadastro - 1

---
+++ cadastro.php
@@ -0,0 +1,21 @@
+<?php
+	
+?>
+<html>
+<head>
+	
+</head>
+<body>
+	<div class="main-container-register">
+		<form method="POST">
+			<label for="input-name" placeholder="Ex: João Silva">Nome completo</label>
+				<input id="input-name" name="name" type="text"/>
+			<label for="input-username" placeholder="Ex: joaosilva">Nome de usuário</label>
+				<input id="input-username" name="username" type="text">
+			<label for="input-password">Senha</label>
+				<input id="input-password" name="password" type="text"/>
+			<button type="submit">Cadastrar</button>
+		</form>
+	</div>
+</body>
+</html>
