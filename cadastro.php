3f42ea46c9fab75d36e17d24696ad688843401cf
Salomão Cruz <salomao.dacostacruz@gmail.com>
2022-05-03 07:15:52 -0400

    Campos de tipo de usuários e justificativa para os usuários na página de cadastro - front

--- cadastro.php
+++ cadastro.php
@@ -12,32 +12,41 @@
 </head>
 <body>
 	<div class="h-100 d-flex justify-content-center aling-items-center">
-	<div class="main-container-register position-absolute align-self-center container w-25 p-5 border rounded">
-		<div class="row w-100">
-			<h4 class="h4-register d-flex align-self-center">Cadastro de Usuário</h4>
-		</div>
-		<div class="row">
-			<form class="form-register form-container" method="post" class="w-100 align-items-center d-flex">
-				<div class="form-box row align-items-center">
-						<label for="input-name" class="label-register">Nome completo</label>
-							<input id="input-name" class="w-75" name="name" type="text"  placeholder="Ex: João Silva"/>
-				</div>
-				<div class="form-box row align-items-center">
-						<label for="input-username"  class="label-register">Nome de usuário</label>
-							<input id="input-username" class="w-75" name="username" type="text" placeholder="Ex: joaosilva"/>
-				</div>
-				<div class="form-box row align-items-center">
-					<label for="input-password"  class="label-register">Senha</label>
-						<input id="input-password" class="w-75" name="password" type="text" placeholder="**********"/>
-				</div>
-				<div class="form-box row align-items-center mt-4">
-					<div class="w-25 flex align-self-center">
-						<button type="submit" class="btn btn-success w-100 p-2">Cadastrar</button>
+		<div class="position-absolute align-self-center container w-25 p-5 border rounded">
+			<div class="row w-100">
+				<h4 class="h4-register d-flex align-self-center">Cadastro de Usuário</h4>
+			</div>
+			<div class="row">
+				<form class="form-register form-container" method="post" class="w-100 align-items-center d-flex">
+					<div class="form-box row align-items-center w-100">
+							<label for="input-name" class="label-register">Nome completo</label>
+								<input id="input-name" class="w-75" name="name" type="text"  placeholder="Ex: João Silva"/>
 					</div>
-				</div>
-			</form>
+					<div class="form-box row align-items-center w-100">
+							<label for="input-username"  class="label-register">Nome de usuário</label>
+								<input id="input-username" class="w-75" name="username" type="text" placeholder="Ex: joaosilva"/>
+					</div>
+					<div class="form-box row align-items-center w-100">
+						<label for="input-password"  class="label-register">Senha</label>
+							<input id="input-password" class="w-75" name="password" type="text" placeholder="**********"/>
+					</div>
+					<div class="form-box row align-items-center w-100">
+						<label for="select-usertype">Tipo de usuário</label>
+							<select id="select-usertype" class="w-75">
+								<option value="">-- Selecione --</option>
+							</select>
+					</div>
+					<div class="form-box row align-items-center w-100">
+						<label>Justificativas para o usuário</label>	
+					</div>
+					<div class="form-box row align-items-center mt-4 w-100">
+						<div class="w-25 flex align-self-center">
+							<button type="submit" class="btn btn-success w-100 p-2">Cadastrar</button>
+						</div>
+					</div>
+				</form>
+			</div>
 		</div>
-	</div>
 	</div>
 </body>
 </html>
