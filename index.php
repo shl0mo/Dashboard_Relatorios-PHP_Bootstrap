f61bf00dd4df93e56578f234289c417c885ad220
Salomão Cruz <salomao.dacostacruz@gmail.com>
2022-05-08 04:43:19 -0400

    Sidebar do dashboard - 1

--- index.php
+++ index.php
@@ -300,11 +300,52 @@
 		</script>
 	</head>
 	<body>
-		<header>
-			<h1>Dados para o relatório</h1>
+		<header class="mb-0 bg-color-dark">
+			<nav class="navbar navbar-dark">
+			</nav>
 		</header>
-		<div class="main-container">
-			<form method="post" class="form form-group container pt-5 pb-5 w-50">
+		<div class="main-container d-flex flex-direction-row h-100 mb-0">
+			<div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px;">
+			    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+			      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+			      <span class="fs-4">Sidebar</span>
+			    </a>
+			    <hr>
+			    <ul class="nav nav-pills flex-column mb-auto">
+			      <li class="nav-item">
+				<a href="#" class="nav-link active" aria-current="page">
+				  <svg class="bi me-2" width="16" height="16"><use xlink:href="#home"></use></svg>
+				  Home
+				</a>
+			      </li>
+			      <li>
+				<a href="#" class="nav-link text-white">
+				  <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
+				  Dashboard
+				</a>
+			      </li>
+			      <li>
+				<a href="#" class="nav-link text-white">
+				  <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"></use></svg>
+				  Orders
+				</a>
+			      </li>
+			      <li>
+				<a href="#" class="nav-link text-white">
+				  <svg class="bi me-2" width="16" height="16"><use xlink:href="#grid"></use></svg>
+				  Products
+				</a>
+			      </li>
+			      <li>
+				<a href="#" class="nav-link text-white">
+				  <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"></use></svg>
+				  Customers
+				</a>
+			      </li>
+			    </ul>
+			    <hr>
+			</div>
+			<form method="post" class="form form-group container pt-5 pb-5 w-100">
 				<div id="form-container" class="form-container justify-center w-75">
 					<div class="form-box row">
 						<label for="date-input">Data</label>
