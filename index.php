fa4c7363446b4c9f4dfbd7f2ef196838f174ddce
Salomão Cruz <salomao.dacostacruz@gmail.com>
2022-05-09 09:11:10 -0400

    Nome dr. text-light

--- index.php
+++ index.php
@@ -302,7 +302,7 @@
 	<body>
 		<header class="mb-0 bg-dark w-100 p-2" style="height: 100px;">
 			<nav class="navbar navbar-dark d-flex flex-direction-column w-100 align-items-center">
-				<div class="container-fluid col w-100">
+				<div class="container-fluid col w-100 text-light">
 					<h1><?php echo 'Dr(a). '.$_SESSION['session'];?></h1>
 				</div>
 				<div class="container-fluid col-md-1 d-flex w-100">
