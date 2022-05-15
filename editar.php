40b89906df4cd3899c3e387939db25a611b0d02a
Salomão Cruz <salomao.dacostacruz@gmail.com>
2022-05-15 04:42:14 -0400

    Acesso à página de edição apenas se estiver logado

--- editar.php
+++ editar.php
@@ -1,3 +1,17 @@
 <?php
+	session_start();
+	if (!isset($_SESSION['session'])) {
+		header('Location: http://localhost/Relatorios/login.php');
+		exit();
+	} else if ($_SESSION['session'] == 'admin') {
+		header('Location: http://localhost/Relatorios/cadastro.php');
+		exit();
+	}
+	if (isset($_POST['logout'])) {
+		session_destroy();
+		header('Location: http://localhost/Relatorios/login.php');
+		exit();
+	}
+
 	echo '<h1>Editar</h1>';	
 ?>
