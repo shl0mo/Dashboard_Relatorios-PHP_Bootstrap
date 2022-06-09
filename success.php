3995d6ad91f59946899f2ef54052af03f07dc388
Salomão Cruz <salomao.dacostacruz@gmail.com>
2022-06-09 17:25:05 -0400

    Página de sucesso de atualização de cadastro#

--- success.php
+++ success.php
@@ -1,11 +1,24 @@
 <?php require_once('./views/header-sidebar.php');?>
-	<div id="success-container" class="d-flex w-100 justify-content-center flex-column">
-		<div class="w-100 d-flex justify-content-center">
-			<h1>Cadastro efetuado com sucesso</h1>
-		</div>
-		<div class="w-100 d-flex justify-content-center">
-			<button class="btn btn-primary px-2 px-5">OK</button>
-		</div>
+	<?php
+		if (isset($_POST['confirm'])) {
+			header('Location: http://localhost/Relatorios/listar.php');
+		}
+	?>
+	<style>
+		#success-container {
+			margin-top: 100px;
+			box-shadow: 0px 2px 10px rgba(0,0,0,0.2);
+		}
+	</style>
+	<div id="success-container" class="d-flex align-self-center w-75 justify-content-center flex-column rounded py-5">
+		<form method="post">
+			<div class="w-100 d-flex justify-content-center mb-2">
+				<h2>Cadastro efetuado com sucesso</h2>
+			</div>
+			<div class="w-100 d-flex justify-content-center">
+				<button type="submit" name="confirm" class="btn btn-primary px-2 px-5">OK</button>
+			</div>
+		</form>
 	</div>
 </div>
 </html>
