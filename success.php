01bd09c268f6ed24ea8ee2772e453281f6945309
Salomão Cruz <salomao.dacostacruz@gmail.com>
2022-06-09 17:04:09 -0400

    View do header e sidebar; Página de sucesso de atualização de cadastro - 1

---
+++ success.php
@@ -0,0 +1,11 @@
+<?php require_once('./views/header-sidebar.php');?>
+	<div id="success-container" class="d-flex w-100 justify-content-center flex-column">
+		<div class="w-100 d-flex justify-content-center">
+			<h1>Cadastro efetuado com sucesso</h1>
+		</div>
+		<div class="w-100 d-flex justify-content-center">
+			<button class="btn btn-primary px-2 px-5">OK</button>
+		</div>
+	</div>
+</div>
+</html>
